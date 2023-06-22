173a5c542da498956131655c665e900d69d4f3b6
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-06-23 02:07:56 +0900

    점수, 점수UI, 점수 기록, 애니메이션 수정 남음

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	private static GameManager instance;
+	GameObject Tuto;
+	GameObject restart;
+	GameObject startButton;
+	public GameObject Object;
+	public Transform spawnPoint;
+	public float objectInterver;
+	public LayerMask playerLayer;
+	public LayerMask objectLayer;
+	public bool isFirst = false;
+	Coroutine spawn;
 	public float moveSpeed;
+	float initialMoveSpeed;
 	public float jumpPower;
+	public bool isPlay = true;
   // 게임 매니저에 대한 접근을 제공하는 프로퍼티
 	public static GameManager Instance
 	{
@@ -24,5 +37,40 @@ public class GameManager : MonoBehaviour
 
     // 게임 매니저 오브젝트를 파괴하지 않고 유지
     DontDestroyOnLoad(gameObject);
+
+		Physics2D.gravity = Vector3.zero;
+		initialMoveSpeed = moveSpeed;
+	}
+	IEnumerator Spawn(){
+		while(true){
+			Instantiate(Object, spawnPoint.position, Quaternion.identity);
+			yield return new WaitForSeconds(objectInterver);
+		}
+	}
+	public void Play(){
+		isPlay = true;
+		isFirst = false;
+		spawn = StartCoroutine(Spawn());
+		Physics2D.gravity = Vector3.down * 9.8f;
+		Tuto.SetActive(false);
+	}
+	public void Die(){
+		isPlay = false;
+		isFirst = false;
+		moveSpeed = 0;
+		StopCoroutine(spawn);
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Object"), true);
+		restart.SetActive(true);
+	}
+	public void Restart(){
+		Physics2D.gravity = Vector3.zero;
+		moveSpeed = initialMoveSpeed;
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Object"), false);
+		SceneManager.LoadScene(0);
+	}
+	public void UISet(UIManager uI){
+		Tuto = uI.Tuto;
+		restart = uI.restart;
+		startButton = uI.startButton;
 	}
 }
